099a9778f7defe19142b77dd5333304dca589132
RDMillen <rossmillen@protonmail.com>
2023-02-03 11:47:23 +0000

    initial commit

--- Functions/def.py
+++ Functions/def.py
@@ -1,11 +1,56 @@
+# Functions & Subroutines
+# R Millen 2023
+
 import time
 
-my_string = ""
-my_int = 0
-my_float = 1.0
+# Operation in line:
 
-def my_function():
-    total = (int(my_float) + my_int)
-    return total
-print(my_function())
-my_function()
+#print(8 + 5)
+
+# Operating in line using variables:
+
+#my_string = "8"
+#my_int = 5
+
+#total = int(my_string) + my_int
+#print(total)
+
+# Define - Function Name - (Parameters)
+# Out of line code to be called.
+
+#def add_o_matic(a, b):
+#    total = a + b
+#    return(total)
+
+# Call Function in Line - (Arguments)
+#add_o_matic(7, 3)
+
+#add_o_matic_total = add_o_matic(7, 10)
+
+#print(add_o_matic_total)
+
+# What's Wrong? Fix Me!
+# add_o_matic (13, "10")
+
+# Difference between print and return in a function.
+
+#def print_function():
+#    print("I used print")
+
+#def return_function():
+#    return "I used return"
+
+#f1 = print_function()
+#f2 = return_function()
+
+#print("Lets see what f1 and f2 show...")
+
+#time.sleep(2)
+
+#print(f1)
+#print(f2)
+
+# Summary
+# Print is used for returning a human readable string.
+# Return provides a usable value that can be stored into a variable or as part
+# of another piece of code. 
