de9cc398a0b508572fab4d63db8750445a13477a
Ross Millen <60128472+RDMillen@users.noreply.github.com>
2023-10-17 18:38:38 +0000

    Update helloworld32.s

--- Resources/Assembly/helloworld32.s
+++ Resources/Assembly/helloworld32.s
@@ -6,19 +6,19 @@
 
 _start:
 
-	# Write
-  mov %eax, 4
-	mov %ebx, 1
-	lea %ecx, [hello_world]
-	mov %edx, 13
-	int 0x80
+# Write
+mov %eax, 4
+mov %ebx, 1
+lea %ecx, [hello_world]
+mov %edx, 13
+int 0x80
 	
-  # Exit
-  mov %eax, 1 
-  mov %ebx, 65
-  int 0x80
+# Exit
+mov %eax, 1 
+mov %ebx, 65
+int 0x80
 
 # String to stdout
 .section .data
-	hello_world:
-	.ascii "Hello, World\n"
+hello_world:
+.ascii "Hello, World\n"
